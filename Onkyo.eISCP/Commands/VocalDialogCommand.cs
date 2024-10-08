7c38152ec32b85375721794bece1c69ebb94fa5e
nathenxbrewer <nathenbrewer@gmail.com>
2024-10-08 12:58:29 -0400

    Fixed accidental nested namespace

--- Onkyo.eISCP/Commands/VocalDialogCommand.cs
+++ Onkyo.eISCP/Commands/VocalDialogCommand.cs
@@ -1,67 +1,65 @@
+using System.Globalization;
+using System.Threading.Tasks;
+
 namespace Onkyo.eISCP.Commands
 {
-    using System;
-    using System.Globalization;
-    using System.Threading.Tasks;
-
-    namespace Onkyo.eISCP.Commands
+    public enum VocalDialogValues
     {
-        public enum VocalDialogValues
+        Level0 = 0x00,
+        Level1 = 0x01,
+        Level2 = 0x02,
+        Level3 = 0x03,
+        Level4 = 0x04,
+        Level5 = 0x05,
+        UP = 0x06,
+        DOWN = 0x07
+    }
+
+    public class VocalDialogCommand : ISCPMessage
+    {
+        private VocalDialogValues _selectedValue;
+
+        public VocalDialogCommand() : base("VOC")
         {
-            Level0 = 0x00,
-            Level1 = 0x01,
-            Level2 = 0x02,
-            Level3 = 0x03,
-            Level4 = 0x04,
-            Level5 = 0x05
         }
 
-        public class VocalDialogCommand : ISCPMessage
+        public VocalDialogValues SelectedValue
         {
-            private VocalDialogValues _selectedValue;
-
-            public VocalDialogCommand() : base("VOC")
+            get => _selectedValue;
+            set
             {
-            }
-
-            public VocalDialogValues SelectedValue
-            {
-                get => _selectedValue;
-                set
+                if (_selectedValue != value)
                 {
-                    if (_selectedValue != value)
-                    {
-                        _selectedValue = value;
-                        OnPropertyChanged();
-                    }
+                    _selectedValue = value;
+                    OnPropertyChanged();
                 }
             }
-
-            protected override string BuildMessage()
-            {
-                if (string.IsNullOrEmpty(RawData))
-                    return ((int)SelectedValue).ToString("X2");
-                return RawData;
-            }
-
-            public override void ParseFrom(ISCPMessage source)
-            {
-                base.ParseFrom(source);
-                SelectedValue = (VocalDialogValues)int.Parse(RawData, NumberStyles.HexNumber);
-            }
         }
 
-        public static class VocalDialogCommandExtensions
+        protected override string BuildMessage()
         {
-            public static async Task<VocalDialogCommand> SetVocalDialogAsync(this ISCPConnection connection, VocalDialogValues value)
-            {
-                return await connection.SendCommandAsync<VocalDialogCommand>(new VocalDialogCommand { SelectedValue = value });
-            }
+            if (string.IsNullOrEmpty(RawData))
+                return ((int)SelectedValue).ToString("X2");
+            return RawData;
+        }
 
-            public static async Task<VocalDialogCommand> GetVocalDialogAsync(this ISCPConnection connection)
-            {
-                return await connection.SendCommandAsync<VocalDialogCommand>(new VocalDialogCommand { RawData = "QSTN" });
-            }
+        public override void ParseFrom(ISCPMessage source)
+        {
+            base.ParseFrom(source);
+            SelectedValue = (VocalDialogValues)int.Parse(RawData, NumberStyles.HexNumber);
+        }
+    }
+
+    public static class VocalDialogCommandExtensions
+    {
+        public static async Task<VocalDialogCommand> SetVocalDialogAsync(this ISCPConnection connection, VocalDialogValues value)
+        {
+            return await connection.SendCommandAsync<VocalDialogCommand>(new VocalDialogCommand { SelectedValue = value });
+        }
+
+        public static async Task<VocalDialogCommand> GetVocalDialogAsync(this ISCPConnection connection)
+        {
+            return await connection.SendCommandAsync<VocalDialogCommand>(new VocalDialogCommand { RawData = "QSTN" });
         }
     }
 }
